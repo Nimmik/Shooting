b175a7439e0bfa0c0792c9cc216a789bc5f59003
JaeFromPCBANG <mtbnunu@gmail.com>
2015-06-19 15:52:50 +0900

    basic enemy ai

---
+++ Shooting/Assets/Script/EnemyScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = this.gameObject.GetComponent<Rigidbody> ();
+		rb.AddForce (new Vector3 (0, 0, -100));
+
+		var x = Random.Range (-50, 50);
+		var z = Random.Range (-30, 30);
+		rb.AddForce (new Vector3 (x, 0, z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnTriggerStay(Collider other){
+		var dir = this.transform.position - other.transform.position;
+		dir.z = 0;
+		rb.AddForce (dir/2);
+	}
+
+	void OnCollisionEnter(Collision other){
+		Destroy (this.gameObject);
+	}
+
+
+}
---
+++ Shooting/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using GravityShooting;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject Enemy;
+	public float NextSpawnTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (NextSpawnTime <= Time.time){
+			Spawn ();
+			NextSpawnTime = Time.time + Random.Range(Constants.EnemySpawnIntervalFrom,Constants.EnemySpawnIntervalTo);
+		}
+	}
+
+	void Spawn(){
+		var x = Random.Range (Constants.LeftBound, -Constants.LeftBound);
+		Instantiate (Enemy, new Vector3(x,0,Constants.TopOfTheMap),Enemy.transform.rotation);
+	}
+}
