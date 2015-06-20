49774e57e045e9810eb32c3c35803c37d5bda82e
Jae <mtbnunu@gmail.com>
2015-06-20 15:56:49 +0900

    done in 2d world

--- Shooting/Assets/Script/DistanceCheck.cs
+++ Shooting/Assets/Script/DistanceCheck.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class DistanceCheck : MonoBehaviour {
 	Rigidbody rb;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
 		rb = this.transform.parent.GetComponent<Rigidbody> ();
+		anim = this.transform.parent.FindChild("Enemy1").GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,9 @@ public class DistanceCheck : MonoBehaviour {
 		var dir = this.transform.parent.position - other.transform.position;
 		dir.z = 0;
 		rb.AddForce (dir.normalized/4);
+		anim.SetFloat("Rotation", dir.x);
+	}
+	void OnTriggerExit(Collider other){
+		anim.SetFloat("Rotation", 0);
 	}
 }
