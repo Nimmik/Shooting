653b7e7475d4b34664bbbd0b6d480827a43a0422
Kim <mwkim1992@live.com>
2015-06-20 16:36:49 +0900

    Enemy Ai Look at player

--- Shooting/Assets/Script/DistanceCheck.cs
+++ Shooting/Assets/Script/DistanceCheck.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DistanceCheck : MonoBehaviour {
 	Rigidbody rb;
+	bool distanceChecking;
+	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
 		rb = this.transform.parent.GetComponent<Rigidbody> ();
@@ -10,13 +13,37 @@ public class DistanceCheck : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (distanceChecking == false) {
+			Vector3 origin = this.transform.parent.position;
+			Vector3 dirForward = this.transform.parent.TransformDirection(Vector3.forward);
+			Vector3 directionToPlayer = player.transform.position - origin;
+			Vector3 Difference = dirForward - directionToPlayer;
+			if(Difference.magnitude > 1f){
+				if(origin.x > 0)
+					rb.AddForce(new Vector3(-1 ,0 ,0));
+				else
+					rb.AddForce(new Vector3(1 ,0 ,0));
+			}
+			else if(Difference.magnitude < 1f && Difference.magnitude > 0f){
+				rb.velocity = directionToPlayer;
+			}
+		}
 	}
 	
 	
 	void OnTriggerStay(Collider other){
-		var dir = this.transform.parent.position - other.transform.position;
-		dir.z = 0;
-		rb.AddForce (dir.normalized/4);
+		if (!other.gameObject.CompareTag ("Missile")) {
+			var dir = this.transform.parent.position - other.transform.position;
+			dir.z = 0;
+			rb.AddForce (dir.normalized / 4);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		distanceChecking = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		distanceChecking = false;
 	}
 }
